8b0ee2f36b11f4bffc8b9d3505eb4129157e4d3d
Emmanuella Chukwu <emmanuella.chukwu77@gmail.com>
2023-04-21 13:42:26 -0700

    fix errors in file 3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet
+ * main - Prints the alphabet.
  *
  * Return: Always 0 (Success)
  */
@@ -12,9 +12,8 @@ int main(void)
 
 	for (i = 0; i < 52; i++)
 	{
-		putchar(alp[i])
+		putchar(alp[i]);
 	}
 	putchar('\n');
-	return (0)
+	return (0);
 }
-
