5196606906f0e05bd4d0d31d38d8e5b30015b6ad
Emmanuella Chukwu <emmanuella.chukwu77@gmail.com>
2023-04-20 17:42:25 -0700

    trial fix on 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Print string
- * Return: 0 = success
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
