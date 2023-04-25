71c83a9823097e7635ee56aad0f025855909d54b
Emmanuella Chukwu <emmanuella.chukwu77@gmail.com>
2023-04-25 11:21:49 -0700

    fix betty error in file 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,7 +7,7 @@ void print_alphabet(void)
 {
 	char letter;
 
-	for (letter = 'a'letter <= 'z'; letter++)
+	for (letter = 'a'; iletter <= 'z'; letter++)
 	{
 		_putchar(character);
 		_putchar("\n");
