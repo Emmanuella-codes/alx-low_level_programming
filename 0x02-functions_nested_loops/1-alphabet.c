4d3736b57a24a7dc01d09dbb5f6784dfd8eedc17
Emmanuella Chukwu <emmanuella.chukwu77@gmail.com>
2023-04-25 10:00:06 -0700

    file 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabets in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char character;
+
+	for (character = 'a'; character <= 'z'; character++)
+	{
+		_putchar(character);
+		_putchar("\n");
+	}
+}
