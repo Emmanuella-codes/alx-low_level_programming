ddbea0bc72a19dfc59201b48c1fbc99e9abfbd71
Emmanuella Chukwu <emmanuella.chukwu77@gmail.com>
2023-04-25 19:43:01 -0700

    alx task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _abs - computes the absolute value of an integer
+* @y: the int to check
+* Return: the absolute value of int
+*/
+
+int _abs(int y)
+	{
+	if (y >= 0)
+	{
+	return (y);
+	}
+	return (-y);
+}
