8b736c589fb4a3b890bef4a3b6d0bd625c5fa588
Emmanuella Chukwu <emmanuella.chukwu77@gmail.com>
2023-05-04 12:11:27 -0700

    alx task 0 - concatenate two strings

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar.c - writes c to stdout
+ * @c: character
+ * Return: 1 on success 0 on error
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
