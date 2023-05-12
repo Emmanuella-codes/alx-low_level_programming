3f20934331d15df107756d6ff76070ceca8fa8d3
Emmanuella Chukwu <emmanuella.chukwu77@gmail.com>
2023-05-12 16:37:27 -0700

    create ALC C Static libraries base files

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+Readme for ALX C - Static librabries
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes to stdout
+ * @c: char input
+ * Return: Always 0 (Success)
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
