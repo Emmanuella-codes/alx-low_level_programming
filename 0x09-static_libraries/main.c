baa69127f845ef875ac966a32a6f59add7d864de
Emmanuella Chukwu <emmanuella.chukwu77@gmail.com>
2023-05-15 16:27:28 -0700

    more trials

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - print string and quote
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_puts
+	(
+		"\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick"
+	);
+	return (0);
+}
